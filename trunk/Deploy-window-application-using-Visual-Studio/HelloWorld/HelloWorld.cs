d832375bae48e22d52a73470f3b6b2bdd6662bc5
code2code.info <code2code.info@573acfa8-069b-11df-9bc7-f7babce8d823>
2010-01-21 14:53:09 +0000

    add hello world application

---
+++ trunk/Deploy-window-application-using-Visual-Studio/HelloWorld/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class HelloWorld : Form
+    {
+        public HelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("http://code2code.info");
+        }
+    }
+}

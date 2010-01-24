e760ce4a3fe1ad9a4b5c72547d22b08db7b28f1e
code2code.info <code2code.info@573acfa8-069b-11df-9bc7-f7babce8d823>
2010-01-24 06:48:25 +0000

    add GetUrlFromSitemap project

---
+++ trunk/GetUrlFromSitemap/CS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Xml.Linq;
+
+namespace CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string sitemap = client.DownloadString("http://code2code.info/sitemap.xml");
+            XElement element = XElement.Parse(sitemap);
+            var e = from i in element.Elements()
+                    select i.FirstNode;
+            foreach (XNode item in e)
+            {
+                string url = ((XElement)(item)).Value;
+                Console.WriteLine(url);
+            }
+            Console.Read();
+        }
+    }
+}

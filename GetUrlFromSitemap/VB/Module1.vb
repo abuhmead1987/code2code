3bcfa96a69e990d07b05e63a95a93b9e97a886de
code2code.info <code2code.info@573acfa8-069b-11df-9bc7-f7babce8d823>
2010-01-24 06:48:25 +0000

    add GetUrlFromSitemap project

---
+++ GetUrlFromSitemap/VB/Module1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Net
+Imports System.Linq
+
+Module Module1
+
+    Sub Main()
+        Dim client As New WebClient
+        Dim str As String = client.DownloadString("http://code2code.info/sitemap.xml")
+        Dim element As XElement = XElement.Parse(str)
+        Dim e = From i In element.Elements() _
+            Select i.FirstNode
+        For Each item As XNode In e
+            Dim url As String = (DirectCast(item, XElement)).Value
+            Console.WriteLine(url)
+        Next
+        Console.Read()
+    End Sub
+
+End Module

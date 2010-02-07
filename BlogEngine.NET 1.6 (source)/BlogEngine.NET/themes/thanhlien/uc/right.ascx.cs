1ed779768dc7ed2cc0e98c2486e72662b9b89309
code2code.info <code2code.info@573acfa8-069b-11df-9bc7-f7babce8d823>
2010-02-07 17:47:18 +0000

    get yahoo contact dynamic from XML

--- BlogEngine.NET 1.6 (source)/BlogEngine.NET/themes/thanhlien/uc/right.ascx.cs
+++ BlogEngine.NET 1.6 (source)/BlogEngine.NET/themes/thanhlien/uc/right.ascx.cs
@@ -1,13 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Xml;
 
 public partial class themes_thanhlien_uc_right : System.Web.UI.UserControl
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        XmlDocument xDoc = new XmlDocument();
+        xDoc.Load(Server.MapPath("~/App_Data/AccountContact.xml"));
 
+        lstContact.DataSource = xDoc.SelectNodes("//yahoo");
+        lstContact.DataBind();
+
+        //foreach (XmlNode item in xDoc.SelectNodes("//yahoo"))
+        //{
+        //    string name = item.Attributes.GetNamedItem("name").Value.ToString();
+        //    string id = item.Attributes.GetNamedItem("id").Value.ToString();
+        //}
     }
 }

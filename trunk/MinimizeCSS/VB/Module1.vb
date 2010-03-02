58915714707dd4a6ae4ef7c4cd1ead856dcc9f46
code2code.info <code2code.info@573acfa8-069b-11df-9bc7-f7babce8d823>
2010-03-02 17:16:16 +0000

    add MinimizeCSS solution (C# and VB.NET)

---
+++ trunk/MinimizeCSS/VB/Module1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.IO
+Imports System.Text.RegularExpressions
+
+Module MinCSS
+
+    Sub Main()
+        Dim minstyle As String = RemoveWhiteSpaceFromStylesheets(File.ReadAllText("..\..\style.css"))
+        File.WriteAllText("..\..\style-min.css", minstyle)
+        Console.Write("Done!")
+        Console.Read()
+    End Sub
+
+    Public Function RemoveWhiteSpaceFromStylesheets(ByVal body As String) As String
+        body = Regex.Replace(body, "[a-zA-Z]+#", "#")
+        body = Regex.Replace(body, "[\n\r]+\s*", String.Empty)
+        body = Regex.Replace(body, "\s+", " ")
+        body = Regex.Replace(body, "\s?([:,;{}])\s?", "$1")
+        body = body.Replace(";}", "}")
+        body = Regex.Replace(body, "([\s:]0)(px|pt|%|em)", "$1")
+        ' Remove comments from CSS
+        body = Regex.Replace(body, "/\*[\d\D]*?\*/", String.Empty)
+        Return body
+    End Function
+
+End Module

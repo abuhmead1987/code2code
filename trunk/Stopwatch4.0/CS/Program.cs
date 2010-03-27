968d36333ffcb77a3c7aa6c1b3a91b7864700350
code2code.info <code2code.info@573acfa8-069b-11df-9bc7-f7babce8d823>
2010-03-27 07:44:39 +0000

    add

---
+++ trunk/Stopwatch4.0/CS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Thread.Sleep(1000);
+            sw.Stop();
+            string timeElap = sw.ElapsedMilliseconds.ToString();
+            Console.WriteLine(timeElap);
+            Console.Read();
+            sw.Restart();
+            Console.WriteLine(sw.ElapsedMilliseconds.ToString());
+            Console.ReadKey();
+        }
+    }
+}

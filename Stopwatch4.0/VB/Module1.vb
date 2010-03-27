24246b343ff70242527e1f64ff5d5c1c63711deb
code2code.info <code2code.info@573acfa8-069b-11df-9bc7-f7babce8d823>
2010-03-27 07:44:39 +0000

    add

---
+++ Stopwatch4.0/VB/Module1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Diagnostics
+Imports System.Threading
+
+Module Module1
+
+    Sub Main()
+        Dim sw As New Stopwatch()
+        sw.Start()
+        Thread.Sleep(1000)
+        Dim timeEslap As String = sw.ElapsedMilliseconds.ToString()
+        Console.WriteLine(timeEslap)
+        Console.ReadLine()
+        sw.Restart()
+        Dim timeAfterReset As String = sw.ElapsedMilliseconds.ToString() ' display time after reset sw
+        Console.WriteLine(timeAfterReset)
+        Console.ReadLine()
+    End Sub
+
+End Module

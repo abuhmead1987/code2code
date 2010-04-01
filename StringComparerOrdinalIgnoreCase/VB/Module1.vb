6e5bd4f5fc9440d9cba41b5326c4b1da340f5888
code2code.info <code2code.info@573acfa8-069b-11df-9bc7-f7babce8d823>
2010-04-01 15:44:56 +0000

    add String Comparer Ordinal IgnoreCase

---
+++ StringComparerOrdinalIgnoreCase/VB/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim name As String() = {"NeonQuach", "QuachNguyen"}
+        If name.Contains("neonquach", StringComparer.OrdinalIgnoreCase) Then
+            Console.WriteLine("compare with no case-sensitive, neonquach exits in name array")
+        ElseIf name.Contains("neonquach") Then
+            Console.WriteLine("compare with case-sensitive")
+        End If
+        Console.Read()
+    End Sub
+
+End Module

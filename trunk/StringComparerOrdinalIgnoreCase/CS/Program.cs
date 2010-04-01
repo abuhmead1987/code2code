d62f6f6b6b3cef39421ce81123ec1389bf3034a2
code2code.info <code2code.info@573acfa8-069b-11df-9bc7-f7babce8d823>
2010-04-01 15:44:56 +0000

    add String Comparer Ordinal IgnoreCase

---
+++ trunk/StringComparerOrdinalIgnoreCase/CS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = { "NeonQuach", "QuachNguyen" };
+            if (name.Contains("neonquach",StringComparer.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("compare with no case-sensitive, neonquach exits in name array");
+            }
+            else if (name.Contains("neonquach"))
+            {
+                Console.WriteLine("compare with case-sensitive");
+            }
+            Console.Read();
+        }
+    }
+}

1c11f9ec986c0ff6d4d8ae2c9d79a46cd8fcbc9e
code2code.info <code2code.info@573acfa8-069b-11df-9bc7-f7babce8d823>
2010-07-28 16:08:39 +0000

    add

---
+++ ExtractLinkFromUrl/CS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> allUrls = new List<string>();
+            WebClient client = new WebClient();
+            string content = client.DownloadString("http://code2code.info");
+            string pattern = @"(?i)(?s)<a[^>]+?href=""?(?<url>[^""]+)""?>(?<innerHtml>.+?)</a\s*>";
+            MatchCollection result = Regex.Matches(content, pattern);
+
+            foreach (Match match in result)
+            {
+                string url = match.Groups["url"].Value;
+
+                if (url.IndexOf("http://") != -1)
+                {
+                    allUrls.Add(url);
+                }
+                Console.WriteLine(url);
+            }
+            Console.Read();
+        }
+    }
+}

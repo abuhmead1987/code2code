a74c0d2a875863b32da43786d8a6544b4e0b0328
code2code.info <code2code.info@573acfa8-069b-11df-9bc7-f7babce8d823>
2011-01-06 07:25:31 +0000

    add

---
+++ trunk/VerifyUrlAsynchronously/CS/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CS
+{
+    public class Program
+    {
+        ///reference: http://stevesmithblog.com/blog/verify-a-list-of-urls-in-c-asynchronously/
+        public static void Main(string[] args)
+        {
+            List<string> urls = new List<string>() { "http://code2code.info", "http://vinaIphone.com", "http://vinaIphone.net", "http://localhost" };
+            Parallel.ForEach(urls, l => VerifyUrl(l));
+            Console.Read();
+        }
+
+        public static void VerifyUrl(string url)
+        {
+            var request = WebRequest.Create(url) as HttpWebRequest;
+            request.Method = "HEAD";
+            NetworkCredential networkCredential = new NetworkCredential("nguyen.quach", "1111111");
+
+            IWebProxy proxyies = WebRequest.GetSystemWebProxy();
+            proxyies.Credentials = CredentialCache.DefaultCredentials;
+
+            var response = request.GetResponse() as HttpWebResponse;
+            request.Proxy = proxyies;
+            request.Credentials = networkCredential;
+
+            if ((response.StatusCode == HttpStatusCode.OK))
+            {
+                Console.WriteLine(string.Format("{0} worked!",url));
+            }
+            else
+            {
+                Console.WriteLine(string.Format("{0} does not worked!", url));
+            }
+        }
+    }
+}

c8eb2f3537922b40a5e2556d09519137cf195baa
code2code.info <code2code.info@573acfa8-069b-11df-9bc7-f7babce8d823>
2011-01-14 08:26:19 +0000

    add

---
+++ CustomValidation/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    public void ServerCheckGender(object sender, ServerValidateEventArgs args)
+    {
+        args.IsValid = male.Checked ? true : female.Checked ? true : false;
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        if (Page.IsValid)
+        {
+            Response.Write(male.Checked ? "Male" : female.Checked ? "Female" : "You have no chooice");
+        }
+    }
+}
